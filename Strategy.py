6ea15f1ffa76f25654333aefa8548cc9ac067444
Tyson Trauger <traugertyson@gmail.com>
2016-10-21 09:32:30 -0500

    Expanded on Strategy

--- Strategy.py
+++ Strategy.py
@@ -1,21 +1,31 @@
-class strat:
+class Strategy:
     total_avg = {}
     counter = 0
-
-    def mean_strategy(datum):
-        counter +=1
-        for stock in datum:
-            stock_price = stock["query"]["results"]["quote"]["Ask"]
-            total_avg[stock] = (total_avg[stock]*(counter-1)+stock_price)/(counter) #updating the average value
-            high = stock["query"]["results"]["quote"]["daysHigh"]
-            low = stock["query"]["results"]["quote"]["daysLow"]
+    PERCENTAGE = .25
+    def mean_strategy(self,datum):
+        """Uses mean reversion to decide if to buy, sell, or do nothing.
+        Datum is what is returned by a yahoo finance api call on stocks. """
+        decision = []
+        self.counter +=1
+        for stock in datum["query"]["results"]["quote"]:
+            stock_price = stock["Ask"]
+            stock_name = stock["symbol"]
+            if self.total_avg.get(stock_name) != None:
+                self.total_avg[stock_name] = (self.total_avg[stock_name]*(self.counter-1)+float(stock_price))/(self.counter) #updating the average value
+            else:
+                self.total_avg[stock_name] = float(stock_price)
+  #          self.total_avg[stock_name] = float(stock["PreviousClose"])
+            high = float(stock["DaysHigh"])
+            low = float(stock["DaysLow"])
             days_range = high - low
             stock_amount = 1 #for testing purposes buy/sell only one for now
-            if stock_price < (0.25*days_range+total_avg[stock]):
-                decision.append({stock: [2, stock_amount]})  #0 is nothing, 1 is buy, 2 is sell
-            elif stock_price > (0.25*days_range+total_avg[stock]):
-                decision.append({stock: [1, stock_amount]})  #0 is nothing, 1 is buy, 2 is sell
+            buy_range = self.PERCENTAGE*days_range
+            stock_price = float(stock_price)
+            if stock_price > buy_range/2+self.total_avg[stock_name]:
+                decision.append({stock_name: [2, stock_amount]})  #0 is nothing, 1 is buy, 2 is sell
+            elif stock_price < -1*buy_range/2+self.total_avg[stock_name]:
+                decision.append({stock_name: [1, stock_amount]})  #0 is nothing, 1 is buy, 2 is sell
             else:
-                decision.append({stock: [0,0]})
+                decision.append({stock_name: [0,0]})
         return decision
-
+    
