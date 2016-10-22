a7cea14f6295e9922243086ff48802bd28c19e31
Tyson Trauger <traugertyson@gmail.com>
2016-10-22 14:24:52 -0500

    Fixed average calculation

--- Strategy.py
+++ Strategy.py
@@ -1,31 +1,39 @@
+import pandas
 class Strategy:
-    total_avg = {}
-    counter = 0
-    PERCENTAGE = .25
+    _total_avg = {}
+    _counter = 0
+    _PERCENTAGE = .10
+    _AMOUNTDAYS = 10
     def mean_strategy(self,datum):
         """Uses mean reversion to decide if to buy, sell, or do nothing.
         Datum is what is returned by a yahoo finance api call on stocks. """
         decision = []
-        self.counter +=1
+        self._counter +=1
         for stock in datum["query"]["results"]["quote"]:
             stock_price = stock["Ask"]
             stock_name = stock["symbol"]
-            if self.total_avg.get(stock_name) != None:
-                self.total_avg[stock_name] = (self.total_avg[stock_name]*(self.counter-1)+float(stock_price))/(self.counter) #updating the average value
-            else:
-                self.total_avg[stock_name] = float(stock_price)
-  #          self.total_avg[stock_name] = float(stock["PreviousClose"])
-            high = float(stock["DaysHigh"])
-            low = float(stock["DaysLow"])
-            days_range = high - low
+
+            self._total_avg[stock_name] = self.getAverage(self._AMOUNTDAYS,stock_name)
             stock_amount = 1 #for testing purposes buy/sell only one for now
-            buy_range = self.PERCENTAGE*days_range
+            buy_range = self._PERCENTAGE*self._total_avg[stock_name]
             stock_price = float(stock_price)
-            if stock_price > buy_range/2+self.total_avg[stock_name]:
+            if stock_price > buy_range+self._total_avg[stock_name]:
                 decision.append({stock_name: [2, stock_amount]})  #0 is nothing, 1 is buy, 2 is sell
-            elif stock_price < -1*buy_range/2+self.total_avg[stock_name]:
+            elif stock_price < -1*buy_range+self._total_avg[stock_name]:
                 decision.append({stock_name: [1, stock_amount]})  #0 is nothing, 1 is buy, 2 is sell
             else:
                 decision.append({stock_name: [0,0]})
         return decision
-    
+
+    def getAverage(self,amountDays,stock):
+        """Gets the average close value of the stock over the last amountDays"""
+        totals = 0
+        days = 0
+        total = 0
+        with open("./ClosingData/"+stock+"Data.csv") as data:
+            days = pandas.read_csv(data)
+        length = len(days["Close"].values)
+        for x in xrange(length-1,length-amountDays-1,-1):
+            total+=days["Close"][x]
+        totals = total/float(amountDays)
+        return  totals
